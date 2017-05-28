8b36b01009915b3fd3cebf49ccd63714af6e9276
System <none>
2017-05-28 21:34:19 +0000

    😚💸 Updated with Glitch

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {SET_LANGUAGE} from '../actions';
+
+// Add your code for the languageReducer
+const initialState={language:'en'};
+export const languageReducer = (state=initialState, action)=>{
+  if(action.type === SET_LANGUAGE){
+    return Object.assign({}, state, {
+      language: action.language
+    });
+  }
+  return state;
+};
